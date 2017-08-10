e1d6c17d8fe6a5e495782033d896f15e4a89335e
Teodor Sava <savateodorbv@gmail.com>
2017-08-10 18:15:00 +0200

    Job-Datetime

--- JobFinderAPI/Entities/Job.cs
+++ JobFinderAPI/Entities/Job.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 
 namespace JobFinderAPI.Entities
 {
@@ -12,8 +9,8 @@ namespace JobFinderAPI.Entities
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public DateTime DateStart { get; set; }
-        public DateTime DateEnd { get; set; }
+        public DateTime? DateStart { get; set; }
+        public DateTime? DateEnd { get; set; }
         public int Hours { get; set; }
         public decimal Price { get; set; }
         public int CategoryID { get; set; }
---
+++ JobFinderAPI/Migrations/201708101608382_Job-Datetime.cs
@@ -0,0 +1,16 @@
+namespace JobFinderAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class JobDatetime : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
