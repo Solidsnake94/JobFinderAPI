5b422e58c9e356de2e9727decafeb91d2c9af598
mohaimate <mohaimate@gmail.com>
2017-08-10 18:42:41 +0200

    Implemented SOAP service

---
+++ JobFinderAPI/Controllers/CurrencyController.cs
@@ -0,0 +1,28 @@
+﻿using JobFinderAPI.CurrencyConverter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JobFinderAPI.Controllers
+{
+    [RoutePrefix("api/soap")]
+    public class CurrencyController : ApiController
+    {
+        [Route("convert")]
+        [HttpPost]
+        public decimal convert(String fromCurrency, String toCurrency, decimal amount)
+        {
+            ConverterSoapClient converter = new ConverterSoapClient();
+            if (fromCurrency == "EUR")
+            {
+                return (converter.GetCurrencyRate(toCurrency, DateTime.Now)) * amount;
+            }
+            else
+                return (converter.GetCurrencyRate(fromCurrency, DateTime.Now)) / amount;
+        }
+
+    }
+}
